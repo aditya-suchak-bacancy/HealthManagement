af38acfe8cf7dd2327d8f190ba0d2e578c34eba5
Aditya <adityasuchak24@gmail.com>
2021-02-22 12:51:42 +0530

    Add Login and SignUp feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :users, only: [:create, :show]
+  get "/signup", to: "users#new"
+  get "/login", to: "session#new"
+  post "/sessions", to: "session#create"
+  delete "/sessions", to: "session#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
